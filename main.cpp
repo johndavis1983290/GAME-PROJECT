c7ecbc96ac9f897ec2d34dc0b7ca3b6be7dc26c8
johndavis1983290 <tvu19642@gmail.com>
2025-03-30 03:47:16 +0700

    new update

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <ctime>
 #include <SDL_ttf.h>
+#include <SDL_mixer.h>
 #include "def.h"
 #include "Graphic.h"
 #include "game.h"
@@ -55,6 +56,16 @@ int main(int argc, char *argv[])
     Graphics graphics;
     graphics.init();
 
+    if(Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0){
+        SDL_Log("SDL_mixer could not initialize! SDL_mixer Error: %s", Mix_GetError());
+        return 1;
+    }
+    Mix_Music* backgroundMusic = Mix_LoadMUS("music/background_sound.mp3");
+      if(backgroundMusic == nullptr){
+        SDL_Log("Failed to load background music! SDL_mixer Error: %s", Mix_GetError());
+        return 1;
+      }
+
     if(TTF_Init() == -1){
         cout << "TTF_Init Error: " << TTF_GetError() << endl;
         return 1;
@@ -67,9 +78,10 @@ int main(int argc, char *argv[])
     initTimer(graphics.renderer, font);
     initScore(graphics.renderer, font);
 
-    SDL_Texture* background[2];
-    background[0] = graphics.loadTexture("menu.jpg");
-    background[1] = graphics.loadTexture("background_2.png");
+    SDL_Texture* background[3];
+    background[0] = graphics.loadTexture("Background/menu.jpg");
+    background[1] = graphics.loadTexture("Background/background_2.png");
+    background[2] = graphics.loadTexture("Background/SEE YOU NEXT TIME!.png");
     graphics.prepareScene(background[0]);
     graphics.presentScene();
 
@@ -80,7 +92,7 @@ int main(int argc, char *argv[])
     graphics.presentScene();
 
     Mouse mouse;
-        SDL_Texture* main_character = graphics.loadTexture("Main_character(2).png");
+        SDL_Texture* main_character = graphics.loadTexture("main_fish/Main_character(2).png");
         mouse.x = WINDOW_WIDTH / 2;
         mouse.y = WINDOW_HEIGHT / 2;
         render(mouse, graphics, main_character);
@@ -88,48 +100,48 @@ int main(int argc, char *argv[])
         graphics.presentScene();
     main_fish movement;
         movement.currentTexture = main_character;
-        movement.rightTexture[0] = graphics.loadTexture("fish_right(1).png");
-        movement.rightTexture[1] = graphics.loadTexture("fish_right(2).png");
-        movement.rightTexture[2] = graphics.loadTexture("fish_right(3).png");
-        movement.rightTexture[3] = graphics.loadTexture("fish_right(4).png");
-        movement.rightTexture[4] = graphics.loadTexture("fish_right(5).png");
-        movement.rightTexture[5] = graphics.loadTexture("fish_right(6).png");
-        movement.rightTexture[6] = graphics.loadTexture("fish_right(7).png");
-        movement.leftTexture[0] = graphics.loadTexture("fish_left(1).png");
-        movement.leftTexture[1] = graphics.loadTexture("fish_left(2).png");
-        movement.leftTexture[2] = graphics.loadTexture("fish_left(3).png");
-        movement.leftTexture[3] = graphics.loadTexture("fish_left(4).png");
-        movement.leftTexture[4] = graphics.loadTexture("fish_left(5).png");
-        movement.leftTexture[5] = graphics.loadTexture("fish_left(6).png");
-        movement.leftTexture[6] = graphics.loadTexture("fish_left(7).png");
-        movement.upTexture[0] = graphics.loadTexture("fish_right(1).png");
-        movement.upTexture[1] = graphics.loadTexture("fish_right(2).png");
-        movement.upTexture[2] = graphics.loadTexture("fish_right(3).png");
-        movement.upTexture[3] = graphics.loadTexture("fish_right(4).png");
-        movement.upTexture[4] = graphics.loadTexture("fish_right(5).png");
-        movement.upTexture[5] = graphics.loadTexture("fish_right(6).png");
-        movement.upTexture[6] = graphics.loadTexture("fish_right(7).png");
-        movement.downTexture[0] = graphics.loadTexture("fish_left(1).png");
-        movement.downTexture[1] = graphics.loadTexture("fish_left(2).png");
-        movement.downTexture[2] = graphics.loadTexture("fish_left(3).png");
-        movement.downTexture[3] = graphics.loadTexture("fish_left(4).png");
-        movement.downTexture[4] = graphics.loadTexture("fish_left(5).png");
-        movement.downTexture[5] = graphics.loadTexture("fish_left(6).png");
-        movement.downTexture[6] = graphics.loadTexture("fish_left(7).png");
-        movement.right_downTexture[0] = graphics.loadTexture("fish_right(1).png");
-        movement.right_downTexture[1] = graphics.loadTexture("fish_right(2).png");
-        movement.right_downTexture[2] = graphics.loadTexture("fish_right(3).png");
-        movement.right_downTexture[3] = graphics.loadTexture("fish_right(4).png");
-        movement.right_downTexture[4] = graphics.loadTexture("fish_right(5).png");
-        movement.right_downTexture[5] = graphics.loadTexture("fish_right(6).png");
-        movement.right_downTexture[6] = graphics.loadTexture("fish_right(7).png");
-        movement.left_upTexture[0] = graphics.loadTexture("fish_left(1).png");
-        movement.left_upTexture[1] = graphics.loadTexture("fish_left(2).png");
-        movement.left_upTexture[2] = graphics.loadTexture("fish_left(3).png");
-        movement.left_upTexture[3] = graphics.loadTexture("fish_left(4).png");
-        movement.left_upTexture[4] = graphics.loadTexture("fish_left(5).png");
-        movement.left_upTexture[5] = graphics.loadTexture("fish_left(6).png");
-        movement.left_upTexture[6] = graphics.loadTexture("fish_left(7).png");
+        movement.rightTexture[0] = graphics.loadTexture("main_fish/fish_right(1).png");
+        movement.rightTexture[1] = graphics.loadTexture("main_fish/fish_right(2).png");
+        movement.rightTexture[2] = graphics.loadTexture("main_fish/fish_right(3).png");
+        movement.rightTexture[3] = graphics.loadTexture("main_fish/fish_right(4).png");
+        movement.rightTexture[4] = graphics.loadTexture("main_fish/fish_right(5).png");
+        movement.rightTexture[5] = graphics.loadTexture("main_fish/fish_right(6).png");
+        movement.rightTexture[6] = graphics.loadTexture("main_fish/fish_right(7).png");
+        movement.leftTexture[0] = graphics.loadTexture("main_fish/fish_left(1).png");
+        movement.leftTexture[1] = graphics.loadTexture("main_fish/fish_left(2).png");
+        movement.leftTexture[2] = graphics.loadTexture("main_fish/fish_left(3).png");
+        movement.leftTexture[3] = graphics.loadTexture("main_fish/fish_left(4).png");
+        movement.leftTexture[4] = graphics.loadTexture("main_fish/fish_left(5).png");
+        movement.leftTexture[5] = graphics.loadTexture("main_fish/fish_left(6).png");
+        movement.leftTexture[6] = graphics.loadTexture("main_fish/fish_left(7).png");
+        movement.upTexture[0] = graphics.loadTexture("main_fish/fish_right(1).png");
+        movement.upTexture[1] = graphics.loadTexture("main_fish/fish_right(2).png");
+        movement.upTexture[2] = graphics.loadTexture("main_fish/fish_right(3).png");
+        movement.upTexture[3] = graphics.loadTexture("main_fish/fish_right(4).png");
+        movement.upTexture[4] = graphics.loadTexture("main_fish/fish_right(5).png");
+        movement.upTexture[5] = graphics.loadTexture("main_fish/fish_right(6).png");
+        movement.upTexture[6] = graphics.loadTexture("main_fish/fish_right(7).png");
+        movement.downTexture[0] = graphics.loadTexture("main_fish/fish_left(1).png");
+        movement.downTexture[1] = graphics.loadTexture("main_fish/fish_left(2).png");
+        movement.downTexture[2] = graphics.loadTexture("main_fish/fish_left(3).png");
+        movement.downTexture[3] = graphics.loadTexture("main_fish/fish_left(4).png");
+        movement.downTexture[4] = graphics.loadTexture("main_fish/fish_left(5).png");
+        movement.downTexture[5] = graphics.loadTexture("main_fish/fish_left(6).png");
+        movement.downTexture[6] = graphics.loadTexture("main_fish/fish_left(7).png");
+        movement.right_downTexture[0] = graphics.loadTexture("main_fish/fish_right(1).png");
+        movement.right_downTexture[1] = graphics.loadTexture("main_fish/fish_right(2).png");
+        movement.right_downTexture[2] = graphics.loadTexture("main_fish/fish_right(3).png");
+        movement.right_downTexture[3] = graphics.loadTexture("main_fish/fish_right(4).png");
+        movement.right_downTexture[4] = graphics.loadTexture("main_fish/fish_right(5).png");
+        movement.right_downTexture[5] = graphics.loadTexture("main_fish/fish_right(6).png");
+        movement.right_downTexture[6] = graphics.loadTexture("main_fish/fish_right(7).png");
+        movement.left_upTexture[0] = graphics.loadTexture("main_fish/fish_left(1).png");
+        movement.left_upTexture[1] = graphics.loadTexture("main_fish/fish_left(2).png");
+        movement.left_upTexture[2] = graphics.loadTexture("main_fish/fish_left(3).png");
+        movement.left_upTexture[3] = graphics.loadTexture("main_fish/fish_left(4).png");
+        movement.left_upTexture[4] = graphics.loadTexture("main_fish/fish_left(5).png");
+        movement.left_upTexture[5] = graphics.loadTexture("main_fish/fish_left(6).png");
+        movement.left_upTexture[6] = graphics.loadTexture("main_fish/fish_left(7).png");
 
     const int NUM_FISH1 = 5;
     otherfish_movement movement1[NUM_FISH1];
@@ -139,19 +151,19 @@ int main(int argc, char *argv[])
        movement1[i].height = 45;
        movement1[i].resetFishPosition((rand() % 2 == 0) ? "left" : "right");
 
-       fish1[i].currentTexture = graphics.loadTexture("otherfish_right(1).png");
-       fish1[i].leftTexture[0] = graphics.loadTexture("otherfish_left(1).png");
-       fish1[i].leftTexture[1] = graphics.loadTexture("otherfish_left(2).png");
-       fish1[i].leftTexture[2] = graphics.loadTexture("otherfish_left(3).png");
-       fish1[i].leftTexture[3] = graphics.loadTexture("otherfish_left(4).png");
-       fish1[i].leftTexture[4] = graphics.loadTexture("otherfish_left(5).png");
-       fish1[i].leftTexture[5] = graphics.loadTexture("otherfish_left(6).png");
-       fish1[i].rightTexture[0] = graphics.loadTexture("otherfish_right(1).png");
-       fish1[i].rightTexture[1] = graphics.loadTexture("otherfish_right(2).png");
-       fish1[i].rightTexture[2] = graphics.loadTexture("otherfish_right(3).png");
-       fish1[i].rightTexture[3] = graphics.loadTexture("otherfish_right(4).png");
-       fish1[i].rightTexture[4] = graphics.loadTexture("otherfish_right(5).png");
-       fish1[i].rightTexture[5] = graphics.loadTexture("otherfish_right(6).png");
+       fish1[i].currentTexture = graphics.loadTexture("other_fish/otherfish_right(1).png");
+       fish1[i].leftTexture[0] = graphics.loadTexture("other_fish/otherfish_left(1).png");
+       fish1[i].leftTexture[1] = graphics.loadTexture("other_fish/otherfish_left(2).png");
+       fish1[i].leftTexture[2] = graphics.loadTexture("other_fish/otherfish_left(3).png");
+       fish1[i].leftTexture[3] = graphics.loadTexture("other_fish/otherfish_left(4).png");
+       fish1[i].leftTexture[4] = graphics.loadTexture("other_fish/otherfish_left(5).png");
+       fish1[i].leftTexture[5] = graphics.loadTexture("other_fish/otherfish_left(6).png");
+       fish1[i].rightTexture[0] = graphics.loadTexture("other_fish/otherfish_right(1).png");
+       fish1[i].rightTexture[1] = graphics.loadTexture("other_fish/otherfish_right(2).png");
+       fish1[i].rightTexture[2] = graphics.loadTexture("other_fish/otherfish_right(3).png");
+       fish1[i].rightTexture[3] = graphics.loadTexture("other_fish/otherfish_right(4).png");
+       fish1[i].rightTexture[4] = graphics.loadTexture("other_fish/otherfish_right(5).png");
+       fish1[i].rightTexture[5] = graphics.loadTexture("other_fish/otherfish_right(6).png");
       }
     const int NUM_FISH2 = 5;
     otherfish_movement movement2[NUM_FISH2];
@@ -161,21 +173,22 @@ int main(int argc, char *argv[])
        movement2[i].height = 120;
        movement2[i].resetFishPosition((rand() % 2 == 0) ? "left" : "right");
 
-       fish2[i].currentTexture = graphics.loadTexture("otherfish(2)_right(1).png");
-       fish2[i].leftTexture[0] = graphics.loadTexture("otherfish(2)_left(1).png");
-       fish2[i].leftTexture[1] = graphics.loadTexture("otherfish(2)_left(2).png");
-       fish2[i].leftTexture[2] = graphics.loadTexture("otherfish(2)_left(3).png");
-       fish2[i].leftTexture[3] = graphics.loadTexture("otherfish(2)_left(4).png");
-       fish2[i].leftTexture[4] = graphics.loadTexture("otherfish(2)_left(5).png");
-       fish2[i].leftTexture[5] = graphics.loadTexture("otherfish(2)_left(6).png");
-       fish2[i].rightTexture[0] = graphics.loadTexture("otherfish(2)_right(1).png");
-       fish2[i].rightTexture[1] = graphics.loadTexture("otherfish(2)_right(2).png");
-       fish2[i].rightTexture[2] = graphics.loadTexture("otherfish(2)_right(3).png");
-       fish2[i].rightTexture[3] = graphics.loadTexture("otherfish(2)_right(4).png");
-       fish2[i].rightTexture[4] = graphics.loadTexture("otherfish(2)_right(5).png");
-       fish2[i].rightTexture[5] = graphics.loadTexture("otherfish(2)_right(6).png");
+       fish2[i].currentTexture = graphics.loadTexture("other_fish/otherfish(2)_right(1).png");
+       fish2[i].leftTexture[0] = graphics.loadTexture("other_fish/otherfish(2)_left(1).png");
+       fish2[i].leftTexture[1] = graphics.loadTexture("other_fish/otherfish(2)_left(2).png");
+       fish2[i].leftTexture[2] = graphics.loadTexture("other_fish/otherfish(2)_left(3).png");
+       fish2[i].leftTexture[3] = graphics.loadTexture("other_fish/otherfish(2)_left(4).png");
+       fish2[i].leftTexture[4] = graphics.loadTexture("other_fish/otherfish(2)_left(5).png");
+       fish2[i].leftTexture[5] = graphics.loadTexture("other_fish/otherfish(2)_left(6).png");
+       fish2[i].rightTexture[0] = graphics.loadTexture("other_fish/otherfish(2)_right(1).png");
+       fish2[i].rightTexture[1] = graphics.loadTexture("other_fish/otherfish(2)_right(2).png");
+       fish2[i].rightTexture[2] = graphics.loadTexture("other_fish/otherfish(2)_right(3).png");
+       fish2[i].rightTexture[3] = graphics.loadTexture("other_fish/otherfish(2)_right(4).png");
+       fish2[i].rightTexture[4] = graphics.loadTexture("other_fish/otherfish(2)_right(5).png");
+       fish2[i].rightTexture[5] = graphics.loadTexture("other_fish/otherfish(2)_right(6).png");
       }
     bool quit = false;
+    bool gameOver = false;
     SDL_Event event;
     KeyPressSurfaces lastHorizontalSurfaces = RIGHT;
     while(!quit){
@@ -184,9 +197,14 @@ int main(int argc, char *argv[])
                 quit = true;
             }
         }
+    if(!gameOver){
     KeyPressSurfaces direction = lastHorizontalSurfaces;
     updateTimer(graphics.renderer, font);
     updateScore(graphics.renderer, font);
+    if(timer <=0){
+        timer = 0;
+        gameOver = true;
+    }
     const Uint8* currentKeyStates = SDL_GetKeyboardState(NULL);
 
     if(currentKeyStates[SDL_SCANCODE_UP]){
@@ -250,13 +268,23 @@ int main(int argc, char *argv[])
     graphics.renderTexture(fish2[i].currentTexture, movement2[i].x, movement2[i].y, movement2[i].width, movement2[i].height, graphics.renderer);
     if (checkCollision(mouse.x, mouse.y, 100, 90, movement1[i].x, movement1[i].y, movement1[i].width, movement1[i].height, lastHorizontalSurfaces)) {
                 movement1[i].resetFishPosition((rand() % 2 == 0) ? "left" : "right");
-                timer += 3;
+                timer += 2;
                 score += 10;
             }
     if (checkCollision(mouse.x, mouse.y, 100, 90, movement2[i].x, movement2[i].y, movement2[i].width, movement2[i].height, lastHorizontalSurfaces)) {
                 score = 0;
+                gameOver = true;
+            }
+    }
+    }
+    else{
+        graphics.prepareScene(background[2]);
+        graphics.presentScene();
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
                 quit = true;
             }
+        }
     }
     RenderTime(graphics.renderer);
     RenderScore(graphics.renderer);
